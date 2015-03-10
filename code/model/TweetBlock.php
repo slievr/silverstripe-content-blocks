ee07cd0c3e95f868f370f58360ccdf5673ca0d20
Ethan Brodie <pacpellet@gmail.com>
2015-03-10 10:00:15 +0000

    Update TweetBlock.php

--- code/model/TweetBlock.php
+++ code/model/TweetBlock.php
@@ -2,7 +2,7 @@
 
 class TweetBlock extends Block{
 	static $db = array(
-
+		'TwitterUser' => 'VarChar(255)'
 	);
 
 	static $singular_name = "Tweet Block";
@@ -10,7 +10,9 @@ class TweetBlock extends Block{
 
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
-
+		
+		$fields->addFieldToTab('Root.Main',TextField::create('TwitterUser'));
+		
 		return $fields;
 	}
 
